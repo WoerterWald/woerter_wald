c1a6f7f3bb5e45bdb506c4b7ed3886fdd4444ee8
Max Sommerfeld <max.sommerfeld@spreadgroup.com>
2024-05-04 20:08:15 +0200

    :sparkles: Add prettier, footer, Sass
    - Add trivago import sort
    - Add SASS variables

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+import { Footer } from '@/components/Footer/Footer';
+import '../styles/reset.scss';
+
 export default function Home() {
   return (
-   <h1>Hello from WörterWald</h1>
+    <>
+      <h1>Hello from WörterWald</h1>
+      <Footer />
+    </>
   );
 }
