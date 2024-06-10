116709e9dce0d74c05096786a72b5eb9f28f62fe
Max Sommerfeld <max_sommerfeld@web.de>
2024-06-10 20:41:35 +0200

    :bug: Fix callStack error

--- src/app/actions/getGame.tsx
+++ src/app/actions/getGame.tsx
@@ -4,7 +4,12 @@ import Game from '@/models/Game';
 export const getGame = async () => {
   try {
     await dbConnect();
-    return await Game.findOne({}).sort({ createdAt: -1 }).populate('matchedWords panagrams');
+    const data = await Game.findOne({})
+      .sort({ createdAt: -1 })
+      .populate('matchedWords panagrams')
+      .lean();
+
+    return JSON.parse(JSON.stringify(data));
   } catch (error) {
     console.error(error);
     //TODO: Do something here like redirect to an error or not-found page
