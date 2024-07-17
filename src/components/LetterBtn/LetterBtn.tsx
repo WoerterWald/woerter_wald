ea676af446bcc6c6d6809fd2ac85d2966e4ddaa4
Max Sommerfeld <max_sommerfeld@web.de>
2024-07-17 19:32:30 +0200

    :sparkles: Add leaves and animations
    - Add leaves
    - Add canvas-confetti
    - Rework layout for landscape
      - don't set header and footer to fixed
      - allow scroll if height gets too small
      - disable rubber-band scroll effect
    - Adjust toast position on mobile

--- src/components/LetterBtn/LetterBtn.tsx
+++ src/components/LetterBtn/LetterBtn.tsx
@@ -1,16 +1,19 @@
 import { MouseEvent } from 'react';
 import classNames from 'classnames';
+import Image, { StaticImageData } from 'next/image';
 import styles from './letterBtn.module.scss';
 
 type LetterBtnProps = {
   variant: 'primary' | 'secondary';
   letter: string;
+  leaf?: StaticImageData;
   onClick: (event: MouseEvent<HTMLElement>) => void;
 };
 
-export const LetterBtn = ({ variant, letter, onClick }: LetterBtnProps) => {
+export const LetterBtn = ({ variant, letter, onClick, leaf }: LetterBtnProps) => {
   return (
     <div className={styles.flex}>
+      {leaf && <Image className={styles.leaf} src={leaf} alt="leaf" width={300} height={600} />}
       <button
         className={classNames(
           styles.flex,
