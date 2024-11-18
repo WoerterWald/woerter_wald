991be7d745e1b7a148d0797f34c11b54f401efff
ania <aniarafeld@o2.pl>
2024-11-18 23:25:45 +0100

    adds bcg-color to panagrams in the dropdown list (#20)

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -4,21 +4,18 @@ import { useState } from 'react';
 import { BiSolidDownArrow } from 'react-icons/bi';
 import { BiSolidUpArrow } from 'react-icons/bi';
 import classNames from 'classnames';
+import { Word } from '../Game/Game';
 import styles from './dropdown.module.scss';
 
 type DropdownProps = {
   foundWords: string[];
+  panagrams: Word[];
 };
 
-export const Dropdown = ({ foundWords }: DropdownProps) => {
+export const Dropdown = ({ foundWords, panagrams }: DropdownProps) => {
   const [open, setOpen] = useState(false);
 
   const recentGuesses = foundWords.slice(0).reverse().join(' ').slice(0, 41).concat('...');
-
-  const guessesList = foundWords
-    .slice(0)
-    .sort((a, b) => a.slice(0).toLowerCase().localeCompare(b.slice(0).toLowerCase()))
-    .join(' ');
 
   const handleClick = () => setOpen(!open);
 
@@ -43,7 +40,13 @@ export const Dropdown = ({ foundWords }: DropdownProps) => {
 
       {open && (
         <div className={styles.dropdownContent} onClick={handleClick}>
-          {guessesList}
+          {foundWords
+            .sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()))
+            .map((word) => {
+              const panagram = panagrams.find((elem) => elem.word === word);
+
+              return panagram ? <p className={styles.panagram}>{word}</p> : <p>{word}</p>;
+            })}
         </div>
       )}
     </div>
