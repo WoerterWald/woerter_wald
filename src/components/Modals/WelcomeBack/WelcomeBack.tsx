995c0210e957cf7e69aefdf088740c408dcbb18b
Max Sommerfeld <max_sommerfeld@web.de>
2025-02-18 19:31:48 +0100

    sparkles: Add countdown to welcome back modal

--- src/components/Modals/WelcomeBack/WelcomeBack.tsx
+++ src/components/Modals/WelcomeBack/WelcomeBack.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import Image from 'next/image';
 import { findLevels } from '@/utils/findLevelHelper';
 import { useFindWords } from '@/hooks/useFindWords';
@@ -11,9 +12,34 @@ type WelcomeBack = {
   panagrams: Word[];
 };
 
+const getSecondsUntilMidnight = () => {
+  const now = Date.now();
+  const midnight = new Date().setHours(24, 0, 0, 0);
+
+  return Math.max(0, Math.floor((midnight - now) / 1000));
+};
+
+function formatTime(seconds: number) {
+  const hours = Math.floor(seconds / 3600);
+  const minutes = Math.floor((seconds % 3600) / 60);
+  const secs = seconds % 60;
+  return `${hours}h ${minutes}m ${secs}s`;
+}
+
 export const WelcomeBack = ({ game, panagrams }: WelcomeBack) => {
   const { foundWords, curScore } = useFindWords(game._id, panagrams);
+  const [timeLeft, setTimeLeft] = useState(getSecondsUntilMidnight());
   const { curLevel } = findLevels(game.levels, curScore);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTimeLeft(getSecondsUntilMidnight());
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
 
   return (
     <Modal isSmall>
@@ -37,7 +63,7 @@ export const WelcomeBack = ({ game, panagrams }: WelcomeBack) => {
         </p>
         <div>
           <p>Wie viele Wörter findest du noch bis zum nächsten Rätsel in:</p>
-          <p className={styles.countdown}>7h 12m 45s</p>
+          <p className={styles.countdown}>{formatTime(timeLeft)}</p>
         </div>
       </div>
     </Modal>
